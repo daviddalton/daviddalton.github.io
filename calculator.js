5df5af89d3c4aa36aeac3b97d042b0d8e7f76e82
David Dalton <David@Davids-MacBook-Pro.local>
2016-01-02 15:22:05 -0500

    Initial Commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+function calculate() {
+        var distance = document.getElementById("distance_box").value;
+        var hours = document.getElementById("hours").value;
+        var minutes = document.getElementById("minutes").value;
+        var seconds = document.getElementById("seconds").value;
+        var totalSeconds = (hours * 3600) + (minutes * 60) + +seconds;
+        var paceInSeconds = totalSeconds/distance;
+        var pace = paceInSeconds/60
+        document.getElementById("pace_box").value = (displayMin(pace) + ":" + displaySec(pace));
+}
+
+function displayMin(pace) {
+	return Math.floor(pace);
+}
+
+function displaySec(pace) {
+	var decimal = pace - displayMin(pace);
+	return pad(Math.round(decimal * 60));
+}
+
+function pad(num) {
+    var decimal = num+"";
+
+    while (decimal.length < 2) {
+    	decimal = "0" + decimal;
+    }
+
+    return decimal;
+}
