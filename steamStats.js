012e9eb38b056e95b92b8bf1208a9c56d21825dd
daviddalton <david@chaddalton.com>
2017-10-24 02:12:16 -0400

    first steamStats commit.

---
+++ steamStats.js
@@ -0,0 +1,11 @@
+var api_key = "8ABB0206BE9663B7C477AFACEF49846F";
+var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=8ABB0206BE9663B7C477AFACEF49846F&steamids=76561198291618514"
+function showUserInfo(url) {
+  $.getJSON(url, function(players) {
+    $.each(players[0].steamid, function(i, steamid) {
+      var steamId = steamid;
+      console.log(players);
+        document.getElementById("test").innerHTML = steamId;
+    });
+  });
+}
